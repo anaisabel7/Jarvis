b171ac6439a0f67f0cf05db43adc1333cb419e71
Ana Isabel <imacswv@gmail.com>
2017-06-04 10:05:41 +0200

    Trying a different approach to importing and using Jarvis

--- Jarvis/tests/test_default.py
+++ Jarvis/tests/test_default.py
@@ -1,30 +1,29 @@
 import unittest
-from Jarvis import Jarvis
 from mock import patch
+import Jarvis
 
 
 class DefaultTest(unittest.TestCase):
 
     def setUp(self):
-        self.jarvis = Jarvis()
         self.action = {"enable": ("sound",)}
 
     # check that default is called if the second word of a two word command is not found in
     # the words_remaining ("enable cat and dog" instead of "enable your sound please")
     def test_call_default_for_wrong_dict_cmd(self):
-        with patch.object(self.jarvis, 'default') as default_mock:
-            self.jarvis._generate_output_if_dict(self.action, "enable", ["cat", "and", "dog"])
+        with patch.object(Jarvis.Jarvis, 'default') as default_mock:
+            Jarvis.Jarvis()._generate_output_if_dict(self.action, "enable", ["cat", "and", "dog"])
             self.assertTrue(default_mock.called)
 
     def test_dont_call_default_for_correct_dict_cmd(self):
-        with patch.object(self.jarvis, 'default') as default_mock:
-            self.jarvis._generate_output_if_dict(self.action, "enable", ["your", "sound", "please"])
+        with patch.object(Jarvis.Jarvis, 'default') as default_mock:
+            Jarvis.Jarvis()._generate_output_if_dict(self.action, "enable", ["your", "sound", "please"])
             self.assertFalse(default_mock.called)
 
     # check that default is called if the first word of a several word command is used alone
     def test_call_default_if_1_word_only_dict_cmd(self):
-        with patch.object(self.jarvis, 'default') as default_mock:
-            self.jarvis.precmd("enable")
+        with patch.object(Jarvis.Jarvis, 'default') as default_mock:
+            Jarvis.Jarvis().precmd("enable")
             self.assertTrue(default_mock.called)
 
     # check that default is called if the first word of a several word command is used
@@ -33,12 +32,12 @@ class DefaultTest(unittest.TestCase):
         args = (self.action, "enable", [])
 
         # check that _generate_output_if_dict is called with the expected arguments
-        with patch.object(self.jarvis, '_generate_output_if_dict') as generate_output_mock:
-            self.jarvis.precmd("please, could you enable")
+        with patch.object(Jarvis.Jarvis, '_generate_output_if_dict') as generate_output_mock:
+            Jarvis.Jarvis().precmd("please, could you enable")
             self.assertTrue(generate_output_mock.called)
             generate_output_mock.assert_called_once_with(*args)
 
         # check that _generate_output_if_dict calls default given these arguments
-        with patch.object(self.jarvis, 'default') as default_mock:
-            self.jarvis._generate_output_if_dict(*args)
+        with patch.object(Jarvis.Jarvis, 'default') as default_mock:
+            Jarvis.Jarvis()._generate_output_if_dict(*args)
             self.assertTrue(default_mock.called)
